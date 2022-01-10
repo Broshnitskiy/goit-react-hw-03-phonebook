0f027a277b67e5ad0db84855889d24396dae1d9f
Roman Broshnitskiy <romario777v8@gmail.com>
2022-01-10 23:34:25 +0200

    добавляє початкову форму

--- src/App.js
+++ src/App.js
@@ -8,23 +8,49 @@ class App extends Component {
     name: '',
   };
 
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState(prevState => {
+      const newContact = {
+        name: prevState.name,
+        id: nanoid(),
+      };
+      return {
+        contacts: [...prevState.contacts, newContact],
+      };
+    });
+  };
+
   render() {
     return (
-      <div>
+      <section>
         <h1>Phonebook</h1>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <button type="button">Add contacts</button>
-        <h2>Contacts</h2>
-        <ul>
-          <li></li>
-        </ul>
-      </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </section>
     );
   }
 }
