c49dddb9f98d04932ea3f8523a0871b91feb92ce
Roman Broshnitskiy <romario777v8@gmail.com>
2022-01-11 23:55:39 +0200

    add search input

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
+    filter: '',
   };
 
   handleChange = e => {
@@ -15,13 +21,12 @@ class App extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: this.state.name,
+      number: this.state.number,
+    };
     this.setState(prevState => {
-      const newContact = {
-        id: nanoid(),
-        name: prevState.name,
-        number: prevState.number,
-      };
-
       return {
         contacts: [...prevState.contacts, newContact],
       };
@@ -36,7 +41,18 @@ class App extends Component {
     });
   };
 
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
   render() {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const findContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+
     return (
       <section>
         <h1>Phonebook</h1>
@@ -69,8 +85,18 @@ class App extends Component {
         </form>
         <div>
           <h2>Contacts</h2>
+          <label>
+            Find contacts by name
+            <input
+              type="text"
+              name="filter"
+              value={this.state.filter}
+              onChange={this.handleChange}
+            />
+          </label>
+
           <ul>
-            {this.state.contacts.map(contact => (
+            {findContacts.map(contact => (
               <li key={contact.id}>
                 {contact.name}: {contact.number}
               </li>
