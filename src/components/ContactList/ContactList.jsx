d12a6f9b155b1a982e7b31e70fc0fea258e42acd
Roman Broshnitskiy <romario777v8@gmail.com>
2022-01-23 20:40:16 +0200

    add ContactListItem component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,18 @@
-import PropTypes from 'prop-types';
+import { ContactListItem } from '../ContactListItem/ContactListItem';
 import { List } from './ContactList.styled';
 
 export const ContactList = ({ contacts, onDeleteContact }) => {
   return (
     <List>
       {contacts.map(({ id, name, number }) => (
-        <li key={id}>
-          &#9742; {name}: {number};
-          <button type="button" onClick={() => onDeleteContact(id)}>
-            Delete
-          </button>
-        </li>
+        <ContactListItem
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDeleteContact={onDeleteContact}
+        />
       ))}
     </List>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }),
-  ),
-  onDeleteContact: PropTypes.func.isRequired,
 };
--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -3,19 +3,4 @@ import styled from 'styled-components';
 export const List = styled.ul`
   width: 400px;
   padding: 8px;
-
-  li {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 8px;
-
-    button {
-      cursor: pointer;
-      :hover {
-        color: white;
-        background-color: orangered;
-      }
-    }
-  }
 `;
